a2a54c409627bdf7d0d19cb40216f497a1868e93
SHOAIB FAROOQ <m.shoaib1050@gmail.com>
2020-11-25 00:58:50 +0500

    Complete UI of 2nd Screen in BMI Calculator || lec 69

--- lib/Resultfind.dart
+++ lib/Resultfind.dart
@@ -1,13 +1,65 @@
 import 'package:flutter/material.dart';
+import 'constent_variable.dart';
+import 'Repeate_contanier.dart';
+import 'Input_Page.dart';
 
 class Resultfind extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI RESULT'),
-      ),
-      body: Text("BODY DATA"),
-    );
+        appBar: AppBar(
+          title: Text('BMI RESULT'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Container(
+                child: Center(
+                  child: Text(
+                    'YOUR RESULT',
+                    style: ConstentTextLabe3,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Repeate_contanier(
+                colors: activationcolor,
+                cardWidge: Column(
+                  //crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("NORMAL", style: Result1),
+                    Text("180", style: Result2),
+                    Text(
+                      'SUGGESTION',
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => InputPage()));
+                },
+                child: Container(
+                  child: Center(
+                    child: Text('Re Calculate', style: ConstentTextLabe3),
+                  ),
+                  color: Colors.deepOrange,
+                  height: 60,
+                  width: double.infinity,
+                  margin: EdgeInsets.only(top: 10),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
--- lib/constent_variable.dart
+++ lib/constent_variable.dart
@@ -17,3 +17,13 @@ const ConstentTextLabe3 = TextStyle(
   fontSize: 30,
   fontWeight: FontWeight.w900,
 );
+
+const Result1 = TextStyle(
+  color: Colors.lightBlueAccent,
+  fontSize: 20.0,
+);
+const Result2 = TextStyle(
+  color: Colors.white,
+  fontSize: 80.0,
+  fontWeight: FontWeight.w900,
+);
