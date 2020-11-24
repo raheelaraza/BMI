af6340f04927dcc6b51cb09c072bc10b47864bef
SHOAIB FAROOQ <m.shoaib1050@gmail.com>
2020-11-24 22:16:25 +0500

    Repeate contaner save as new library code in flutter || Lecture 57

--- lib/Input_Page.dart
+++ lib/Input_Page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Repeate_contanier.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -53,19 +54,6 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class Repeate_contanier extends StatelessWidget {
-  Repeate_contanier({@required this.colors});
-  final Color colors;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-          color: colors, borderRadius: BorderRadiusDirectional.circular(15.0)),
     );
   }
 }
