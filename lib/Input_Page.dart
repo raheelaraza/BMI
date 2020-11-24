908f7614946f05597af8f50c70db96624278d109
SHOAIB FAROOQ <m.shoaib1050@gmail.com>
2020-11-24 23:31:32 +0500

    Dart Ternery Operator || Leture 62

--- lib/Input_Page.dart
+++ lib/Input_Page.dart
@@ -17,7 +17,7 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  Color maleColor = deactivationcolor;
+  /*Color maleColor = deactivationcolor;
   Color femaleColor = deactivationcolor;
   void updateColor(Gender genderType) {
     if (genderType == Gender.male) {
@@ -28,7 +28,8 @@ class _InputPageState extends State<InputPage> {
       maleColor = deactivationcolor;
       femaleColor = activationcolor;
     }
-  }
+  }*/
+  Gender selectGender;
 
   @override
   Widget build(BuildContext context) {
@@ -45,11 +46,14 @@ class _InputPageState extends State<InputPage> {
                   child: GestureDetector(
                     onTap: () {
                       setState(() {
-                        updateColor(Gender.male);
+                        //updateColor(Gender.male);
+                        selectGender = Gender.male;
                       });
                     },
                     child: Repeate_contanier(
-                      colors: maleColor,
+                      colors: selectGender == Gender.male
+                          ? activationcolor
+                          : deactivationcolor,
                       cardWidge: RepeateTextIcon(
                         iconData: FontAwesomeIcons.male,
                         Label: "MALE",
@@ -61,11 +65,14 @@ class _InputPageState extends State<InputPage> {
                   child: GestureDetector(
                     onTap: () {
                       setState(() {
-                        updateColor(Gender.female);
+                        //updateColor(Gender.female);
+                        selectGender = Gender.female;
                       });
                     },
                     child: Repeate_contanier(
-                      colors: femaleColor,
+                      colors: selectGender == Gender.female
+                          ? activationcolor
+                          : deactivationcolor,
                       cardWidge: RepeateTextIcon(
                         iconData: FontAwesomeIcons.female,
                         Label: "FEMALE",
