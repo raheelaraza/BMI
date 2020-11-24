6ff475892baccbccb241380bea0d08071c66b3ac
SHOAIB FAROOQ <m.shoaib1050@gmail.com>
2020-11-24 22:12:16 +0500

    How to refactor our code in flutter || Lecture 57

--- lib/Input_Page.dart
+++ lib/Input_Page.dart
@@ -18,49 +18,34 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadiusDirectional.circular(15.0)),
+                  child: Repeate_contanier(
+                    colors: Color(0xFF1D1E33),
                   ),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadiusDirectional.circular(15.0)),
+                  child: Repeate_contanier(
+                    colors: Color(0xFF1D1E33),
                   ),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: Container(
-              margin: EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadiusDirectional.circular(15.0)),
+            child: Repeate_contanier(
+              colors: Color(0xFF1D1E33),
             ),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadiusDirectional.circular(15.0)),
+                  child: Repeate_contanier(
+                    colors: Color(0xFF1D1E33),
                   ),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadiusDirectional.circular(15.0)),
+                  child: Repeate_contanier(
+                    colors: Color(0xFF1D1E33),
                   ),
                 ),
               ],
@@ -68,6 +53,19 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class Repeate_contanier extends StatelessWidget {
+  Repeate_contanier({@required this.colors});
+  final Color colors;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+          color: colors, borderRadius: BorderRadiusDirectional.circular(15.0)),
     );
   }
 }
